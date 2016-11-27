62564080f44105fcfbafe9978e869311bf0bc036
Gianluca Interino <gianluca.interino@ie.ibm.com>
2016-11-27 14:16:35 +0000

    bug fix
    request fix and response to the API added

--- app.js
+++ app.js
@@ -436,8 +436,9 @@ var mqlightClient = mqlight.createClient(opts, function(err) {
 })
 
 app.get('/api/mq/send/:word', function(request, response){
-	var word =req.params.word
+	var word =request.params.word
 	mqlightClient.send('mytopic', word)
+	response.send("", word, "has been published on the topic 'mytopic' ")
 })
 
 http.createServer(app).listen(app.get('port'), '0.0.0.0', function() {
